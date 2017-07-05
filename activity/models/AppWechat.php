872be24ba324147913fb126627fcb491a8f12e22
daniel <djc1103617718@163.com>
2017-07-06 02:29:19 +0800

    login

--- activity/models/AppWechat.php
+++ activity/models/AppWechat.php
@@ -7,7 +7,7 @@ use yii\base\Model;
 
 class AppWechat extends Model
 {
-    const ACCESS_TOKEN_API = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=$s&code=%s&grant_type=authorization_code';
+    const ACCESS_TOKEN_API = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code';
     const LOGIN_API = 'https://open.weixin.qq.com/connect/qrconnect?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=STATE#wechat_redirect';
 
     public $app_id = 'wxd1e5e5eb0b1ef6b4';
