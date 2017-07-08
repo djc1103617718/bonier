64dcd5a873744c7593badf4a1722d9f34a7ce4b8
daniel <djc1103617718@163.com>
2017-07-09 07:03:35 +0800

    dd

--- activity/models/BargainPartner.php
+++ activity/models/BargainPartner.php
@@ -13,7 +13,7 @@ class BargainPartner extends \common\models\BargainPartner
         if (empty($order)) {
             throw new NotFoundHttpException();
         }
-        $sql = "SELECT w.*, b.decrease_price FROM bargainPartner b INNER JOIN wechat w ON w.open_id = b.open_id WHERE b.order_id=$order->id";
+        $sql = "SELECT w.*, b.decrease_price FROM bargain_partner b INNER JOIN wechat w ON w.open_id = b.open_id WHERE b.order_id=$order->id";
         $response = Yii::$app->db->createCommand($sql)->queryAll();
         return $response;
 
