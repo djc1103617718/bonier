357e1daaf97ef9607b4dd688d85f3728a4872562
daniel <djc1103617718@163.com>
2017-07-13 16:11:35 +0800

    up

--- activity/views/site/index.php
+++ activity/views/site/index.php
@@ -168,7 +168,7 @@ document.getElementById("audio1").play();
 			echo '<div class="container stamp" style="margin-top:10px;padding-left:5px;padding-right:5px;width:100%">';
 			echo '<div style="background:#ffffff;padding-top:5px;padding-bottom:5px;margin-top:3px;">';
 			echo "<img src='$baseUrl$url' style='float:left;margin-left:5px' height='100px' width='100px'>";
-			echo "<span class='rotate_right content1' style='position:absolute;top:50px;left:20px;'><font size='5' color='#FF0000'>已售完</font></span>"
+			echo "<span class='rotate_right content1' style='position:absolute;top:50px;left:20px;'><font size='5' color='#FF0000'>已售完</font></span>";
 			echo "<p style='padding-left:115px;color:#000'>{$product['name']}</p>";
 			echo "<p style='padding-left:115px;color:#000;font-size:10px;margin-top:10px'>{$product['participants']}人参与，剩{$product['lave_num']}件</p>";
 			$start_price = $product['start_price']/100;
